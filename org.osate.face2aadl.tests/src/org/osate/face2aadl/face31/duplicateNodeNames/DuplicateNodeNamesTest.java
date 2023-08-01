94ef23cb6553c0b14e78ac550b86ffbc776c2109
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2023-08-01 15:34:03 -0700

    Generate unique names for TransportNodes in FACE 3.1

---
+++ org.osate.face2aadl.tests/src/org/osate/face2aadl/face31/duplicateNodeNames/DuplicateNodeNamesTest.java
@@ -0,0 +1,9 @@
+package org.osate.face2aadl.face31.duplicateNodeNames;
+
+import org.osate.face2aadl.face31.AbstractTranslator31Test;
+
+public class DuplicateNodeNamesTest extends AbstractTranslator31Test {
+	public DuplicateNodeNamesTest() {
+		super("duplicateNodeNames", false, true);
+	}
+}
