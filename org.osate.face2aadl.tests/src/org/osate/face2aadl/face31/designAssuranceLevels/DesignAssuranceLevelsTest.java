0d4b9ed5a6b853da3b1172a877c63c72ecd00578
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2023-11-15 08:28:08 -0800

    Translate the DesignAssuranceLevel property for FACE 3.1

---
+++ org.osate.face2aadl.tests/src/org/osate/face2aadl/face31/designAssuranceLevels/DesignAssuranceLevelsTest.java
@@ -0,0 +1,9 @@
+package org.osate.face2aadl.face31.designAssuranceLevels;
+
+import org.osate.face2aadl.face31.AbstractTranslator31Test;
+
+public class DesignAssuranceLevelsTest extends AbstractTranslator31Test {
+	public DesignAssuranceLevelsTest() {
+		super("designAssuranceLevels", false, true);
+	}
+}
