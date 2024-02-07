97bd69aedc43b32e280f4c53004bdbaa6245f765
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2024-02-07 15:07:00 -0800

    Test cross-IntegrationModel references in series.

---
+++ org.osate.face2aadl.tests/src/org/osate/face2aadl/face31/mergedThreeInSeries/MergedThreeInSeriesTest.java
@@ -0,0 +1,9 @@
+package org.osate.face2aadl.face31.mergedThreeInSeries;
+
+import org.osate.face2aadl.face31.AbstractTranslator31Test;
+
+public class MergedThreeInSeriesTest extends AbstractTranslator31Test {
+	public MergedThreeInSeriesTest() {
+		super("mergedThreeInSeries", false, true);
+	}
+}
