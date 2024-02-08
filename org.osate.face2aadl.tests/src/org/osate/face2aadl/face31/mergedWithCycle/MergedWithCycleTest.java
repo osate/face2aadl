bee4e640f370952cc97aae54a2c0a2eed0e34616
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2024-02-08 11:51:48 -0800

    Test cross-IntegrationModel references with a cycle.

---
+++ org.osate.face2aadl.tests/src/org/osate/face2aadl/face31/mergedWithCycle/MergedWithCycleTest.java
@@ -0,0 +1,9 @@
+package org.osate.face2aadl.face31.mergedWithCycle;
+
+import org.osate.face2aadl.face31.AbstractTranslator31Test;
+
+public class MergedWithCycleTest extends AbstractTranslator31Test {
+	public MergedWithCycleTest() {
+		super("mergedWithCycle", false, true);
+	}
+}
