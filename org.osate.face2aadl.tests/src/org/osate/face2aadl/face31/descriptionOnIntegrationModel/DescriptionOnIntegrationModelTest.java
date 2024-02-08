efd5fab393e60102227359edb02fbf73dd1603c6
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2024-02-08 12:36:22 -0800

    Test description on a single IntegrationModel.

---
+++ org.osate.face2aadl.tests/src/org/osate/face2aadl/face31/descriptionOnIntegrationModel/DescriptionOnIntegrationModelTest.java
@@ -0,0 +1,9 @@
+package org.osate.face2aadl.face31.descriptionOnIntegrationModel;
+
+import org.osate.face2aadl.face31.AbstractTranslator31Test;
+
+public class DescriptionOnIntegrationModelTest extends AbstractTranslator31Test {
+	public DescriptionOnIntegrationModelTest() {
+		super("descriptionOnIntegrationModel", false, true);
+	}
+}
